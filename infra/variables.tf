3b2ee1af51ef305253667ab7b08f65a517005546
EC2 Default User <ec2-user@ip-172-31-41-7.eu-west-1.compute.internal>
2024-11-05 19:23:49 +0000

    minor endringer 3

--- infra/variables.tf
+++ infra/variables.tf
@@ -8,3 +8,13 @@ variable "alarm_email" {
   type        = string
   description = "Email to receive SNS alarm notifications"
 }
+
+variable "prefix" {
+  type        = string
+  description = "Prefiks som brukes i ressursnavn"
+}
+
+variable "image" {
+  type        = string
+  description = "ECR-bildet som brukes for alarm"
+}
